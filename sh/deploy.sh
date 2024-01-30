e750e015aa1eb7f2d75bb1028fb6e11cadbe70ec
Nikhil310892 <110358104+Nikhil310892@users.noreply.github.com>
2024-01-30 14:18:01 -0500

    Update deploy.sh

--- sh/deploy.sh
+++ sh/deploy.sh
@@ -3,4 +3,4 @@ then
     sudo mkdir /var/www/html/WSPstudent                   # create directory
 fi
 sudo cp -r /tmp/deploy/php/* /var/www/html/WSPstudent              #copy PHP files
-sudo chown -R www-data:www-data /var/www/html/WSPstudent/  # give apache server ownership
+sudo chown -R www-data:www-data /var/www/html/WSPstudent/  # give apache server ownershipp
